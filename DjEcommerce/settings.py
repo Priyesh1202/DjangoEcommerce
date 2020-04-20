62d8c780a8455e7656c245b641286f27d4155478
Priyesh1202 <priyesh.vakharia@gmail.com>
2020-04-20 10:55:24 +0530

    Second commit

--- DjEcommerce/settings.py
+++ DjEcommerce/settings.py
@@ -77,13 +77,16 @@ WSGI_APPLICATION = 'DjEcommerce.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'ecommercefinal',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'Pv12022001',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommercefinal',
-        'USER': 'postgres',
-        'PASSWORD': 'Pv12022001',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
